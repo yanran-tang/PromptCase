145232ea8455aebc2757a1baf36e84f6066ffe74
yanran-tang <tang.niann@gmail.com>
2023-08-14 22:19:48 +1000

    initial

---
+++ LeCaRD/lecard_preprocessing.py
@@ -0,0 +1,129 @@
+import json
+import os
+import torch
+from tqdm import tqdm
+
+
+#Load data
+query_dict = {}
+q_issue_dict = {}
+Q_DIR = './LeCaRD/query.json'
+with open(Q_DIR, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        q_dict = json.loads(line)
+        query_dict.update({q_dict['ridx']:q_dict['q']})
+        q_issue_dict.update({q_dict['ridx']:q_dict['crime']})
+    f.close()
+
+candidate_path = './LeCaRD/candidate/'
+
+can_list = []
+candidate_fact_dict = {}
+
+for k,v in tqdm(query_dict.items()):        
+    files = os.listdir(candidate_path+str(k)+'/')
+    candidate_fact_dict.update({k:query_dict[k]})
+    for file in files:
+        # file_name = file.split('.')[0]
+        file_name = str(k)+'/'+file
+        can_list.append(file_name)
+        with open (candidate_path+str(k)+'/'+file, 'r') as f:
+            doc = json.load(f)
+            case_txt = doc['ajjbqk']
+            if '经审理查明' in case_txt:
+                fact_0 = case_txt.split('经审理查明')[1]
+                if '上述事实' in fact_0:
+                    fact_0 = fact_0.split('上述事实')[0]
+            elif '上述事实' in case_txt:
+                fact_0 = case_txt.split('上述事实')[0]
+            else:
+                fact_0 = case_txt
+        if 20 <len(fact_0) < 150:
+            candidate_fact_dict.update({file_name:case_txt})
+        elif len(case_txt) <= 20:
+            candidate_fact_dict.update({file_name:doc['cpfxgc']})
+        else:
+            candidate_fact_dict.update({file_name:fact_0})
+
+with open('./LeCaRD/candidate_fact_dict.json', 'w') as f:
+    json.dump(candidate_fact_dict, f, ensure_ascii=False)
+    f.close()
+
+with open('./LeCaRD/issues_name.txt', 'r') as f:
+    issues  = f.readlines()
+    f.close()
+
+
+issue_name_list = []
+for issue in issues:
+    issue_1 = issue.split('罪\n')[0]
+    if '、' in issue_1:
+        issue_2 = issue_1.split('、')
+        for issue in issue_2:
+            issue_name_list.append(issue)
+    else:
+        issue_name_list.append(issue_1.split('罪\n')[0])
+
+query_label_dict = {}
+with open('./LeCaRD/lecard_golden_labels.json', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        q_dict = json.loads(line)
+        query_label_dict.update(q_dict)
+    f.close()
+
+issue_dict = {}
+for k,v in tqdm(query_label_dict.items()): 
+    files = os.listdir(candidate_path+str(k)+'/')
+    q_issue_txt = ''
+    for value in q_issue_dict[int(k)]:
+        if '、' in value:
+            value = value.split('罪\n')[0]
+            values = value.split('、')
+            for value_1 in values:
+                if value_1 in issue_name_list:
+                    q_issue_txt += value_1+ ' ' 
+        else:
+            q_issue_txt += value.split('罪\n')[0]+ ' '        
+    issue_dict.update({k:q_issue_txt})
+    for file in files:
+
+        file_issue_txt = ''
+        file_name = str(k)+'/'+file  
+        with open (candidate_path+str(k)+'/'+file, 'r') as f:
+            doc = json.load(f)
+            aj_topic = doc['ajName']
+            f.close()       
+        for i in issue_name_list:
+            if i in aj_topic:
+                file_issue_txt += i+' '
+        
+        if len(file_issue_txt) == '':
+            for i in issue_name_list:
+                if i in doc['ajjbqk']:
+                    file_issue_txt += i+' '
+
+        if len(file_issue_txt) == '':
+            for i in issue_name_list:
+                if i in doc['cpfxgc']:
+                    file_issue_txt += i+' '
+        
+        if len(file_issue_txt) == '':
+            for i in issue_name_list:
+                if i in doc['pjjg']:
+                    file_issue_txt += i+' '
+        
+        if len(file_issue_txt) == '':
+            print(k, file_name, 'No issue')
+
+        issue_dict.update({file_name:file_issue_txt})
+
+with open('./LeCaRD/candidate_issue_dict.json', 'w') as f:
+    json.dump(issue_dict, f, ensure_ascii=False)
+    f.close()
+
+print(len(candidate_fact_dict))
+print(len(issue_dict))
+print('Finished.')
+
---
+++ torch_metrics.py
@@ -0,0 +1,62 @@
+import torch
+from torchmetrics import RetrievalNormalizedDCG, RetrievalMRR, RetrievalMAP, RetrievalPrecision
+
+import json
+import os
+
+
+def t_metrics(dataset, predict_path):
+
+    if dataset == 'coliee':
+        label_path = './COLIEE/task1_test_2023/task1_test_labels_2023.json'
+    elif dataset == 'lecard':
+        label_path = './LeCaRD/lecard_golden_labels.json'
+
+    ## prediction preprocess
+    pre_dic = json.load(open(predict_path, 'r'))
+    label_dict = json.load(open(label_path, 'r'))
+
+    index = -1
+    index_list = []
+    preds_list = []
+    traget_list = []
+    for key,value in pre_dic.items():
+        index += 1
+        rank = 1.0
+        for v in value[:5]:
+            index_list.append(index)
+            preds_list.append(rank)
+            if v in label_dict[key]:
+                traget_list.append(True)
+            else:
+                traget_list.append(False)
+            rank -= 0.05
+
+    ## mrr@5
+    mrr_index_list = []
+    mrr_preds_list = []
+    mrr_traget_list = []
+    for key,value in pre_dic.items():
+        index += 1
+        rank = 1.0
+        for v in value[:5]:
+            mrr_index_list.append(index)
+            mrr_preds_list.append(rank)
+            if v in label_dict[key]:
+                mrr_traget_list.append(True)
+            else:
+                mrr_traget_list.append(False)
+            rank -= 0.05
+
+
+    ndcg = RetrievalNormalizedDCG(k=5)
+
+    mrr = RetrievalMRR()
+    map = RetrievalMAP()
+    p = RetrievalPrecision(k=5)
+    ndcg_score = ndcg(torch.tensor(preds_list), torch.tensor(traget_list), indexes=torch.tensor(index_list))
+    mrr_score = mrr(torch.tensor(mrr_preds_list), torch.tensor(mrr_traget_list), indexes=torch.tensor(mrr_index_list)) ##mrr@5
+    map_score = map(torch.tensor(preds_list), torch.tensor(traget_list), indexes=torch.tensor(index_list)) 
+    p_score = p(torch.tensor(preds_list), torch.tensor(traget_list), indexes=torch.tensor(index_list))
+
+    return ndcg_score, mrr_score, map_score, p_score
